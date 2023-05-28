361355e66de2fdef65ea77f0aec01dfc6cbe37c6
Tatiana Makovozova <130184179+tatianamakovozova-qa@users.noreply.github.com>
2023-05-29 03:32:41 +0600

    автотесты для формы логина и пароля

---
+++ qasite.cy.js
@@ -0,0 +1,75 @@
+describe('Тестирование формы логина и пароля', function () {
+    
+    it('Правильный логин, правильный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon')
+         })
+
+
+    it('Восстановление пародя', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon')
+         })
+
+ 
+
+    it('Правильный логин, неправильный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio123');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon')
+        })
+    
+
+    it('Неравильный логин, правильный пароль', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('asdasd@dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon')
+             })
+
+
+    it('Проверка наличия @ в поле логие', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Нужно исправить проблему валидации');
+        cy.get('#exitMessageButton > .exitIcon')
+             })
+  
+     it('Проверка на приведение к строчным буквам', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GeMan@Dolnikov.ru');
+        cy.get('#loginButton').should('be.disabled');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('be.enabled');
+        cy.get('#loginButton').click();
+        cy.contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon')
+               })
+
+
+
+ })        
